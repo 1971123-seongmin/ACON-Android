90c6059f27b300ce812f953cd974cce3501dc922
Thirfir <sangbbang31@gmail.com>
2025-08-30 14:38:15 +0900

    fix: 초기 지역인증에서 건너뛰기 시 다음화면 이동 로직 정상화

--- feature/onboarding/src/main/java/com/acon/feature/onboarding/area/composable/AreaVerificationScreenContainer.kt
+++ feature/onboarding/src/main/java/com/acon/feature/onboarding/area/composable/AreaVerificationScreenContainer.kt
@@ -22,6 +22,8 @@ import org.orbitmvi.orbit.compose.collectSideEffect
 fun AreaVerificationScreenContainer(
     onNavigateToVerifyInMap: () -> Unit,
     onNavigateToChooseDislikes: () -> Unit,
+    onNavigateToIntroduce: () -> Unit,
+    onNavigateToSpotList: () -> Unit,
     skippable: Boolean,
     modifier: Modifier = Modifier,
     viewModel: AreaVerificationViewModel = hiltViewModel(creationCallback = { factory: AreaVerificationViewModel.Factory ->
@@ -73,6 +75,8 @@ fun AreaVerificationScreenContainer(
             }
 
             is AreaVerificationSideEffect.NavigateToChooseDislikes -> onNavigateToChooseDislikes()
+            is AreaVerificationSideEffect.NavigateToIntroduce -> onNavigateToIntroduce()
+            is AreaVerificationSideEffect.NavigateToSpotList -> onNavigateToSpotList()
         }
     }
 
