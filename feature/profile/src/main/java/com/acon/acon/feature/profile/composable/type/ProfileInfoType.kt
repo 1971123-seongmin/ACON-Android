477e28552504da6640971bc9b352b3f789324394
Thirfir <thirfir@naver.com>
2025-09-18 16:59:30 +0900

    chore: 프로필 정보, 수정 레거시 코드 제거

--- feature/profile/src/main/java/com/acon/acon/feature/profile/composable/type/ProfileInfoType.kt
+++
@@ -1,6 +0,0 @@
-package com.acon.acon.feature.profile.composable.type
-
-enum class ProfileInfoType {
-    ACON,
-    AREA
-}
--- feature/profile/src/main/java/com/acon/acon/feature/profile/composable/type/ProfileUpdateResult.kt
+++
@@ -1,6 +0,0 @@
-package com.acon.acon.feature.profile.composable.type
-
-enum class ProfileUpdateResult {
-    SUCCESS,
-    FAILURE
-}
