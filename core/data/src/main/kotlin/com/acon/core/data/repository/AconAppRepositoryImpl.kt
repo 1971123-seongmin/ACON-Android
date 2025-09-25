200192c2e37c651d5ad7582f6d81413ad4ad3c16
Thirfir <thirfir@naver.com>
2025-09-25 11:30:38 +0900

    refactor: 이미지 업로드 레포 함수 병렬 처리

--- core/data/src/main/kotlin/com/acon/core/data/repository/AconAppRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/acon/core/data/repository/AconAppRepositoryImpl.kt
@@ -1,22 +1,29 @@
 package com.acon.core.data.repository
 
 import android.content.Context
+import android.net.Uri
 import androidx.core.net.toUri
 import androidx.documentfile.provider.DocumentFile
+import com.acon.acon.core.common.IODispatcher
 import com.acon.acon.core.model.type.ImageType
-import com.acon.core.data.datasource.remote.AconAppRemoteDataSource
-import com.acon.core.data.error.runCatchingWith
 import com.acon.acon.domain.error.app.FetchShouldUpdateError
 import com.acon.acon.domain.repository.AconAppRepository
+import com.acon.core.data.datasource.remote.AconAppRemoteDataSource
 import com.acon.core.data.dto.request.GetPresignedUrlRequest
+import com.acon.core.data.dto.response.PresignedUrlResponse
+import com.acon.core.data.error.runCatchingWith
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 import javax.inject.Inject
-import kotlin.collections.contains
 
 class AconAppRepositoryImpl @Inject constructor(
     @ApplicationContext private val context: Context,
+    @IODispatcher private val dispatcher: CoroutineDispatcher,
     private val aconAppRemoteDataSource: AconAppRemoteDataSource
 ) : AconAppRepository {
 
@@ -33,25 +40,46 @@ class AconAppRepositoryImpl @Inject constructor(
     override suspend fun uploadImage(imageType: ImageType, url: String): Result<String> {
         return runCatchingWith {
             val contentUri = url.toUri()
-            val fileName = DocumentFile.fromSingleUri(context, contentUri)?.name
-                ?: error("Failed to read file name: $url")
 
-            val presignedUrlResponse = aconAppRemoteDataSource.getPresignedUrl(GetPresignedUrlRequest(
+            return@runCatchingWith withContext(dispatcher) {
+                val presignedUrlResponseDeferred = async {
+                    contentUri.getPresignedUrlResponse(imageType)
+                }
+                val requestBodyDeferred = async {
+                    contentUri.getRequestBody()
+                }
+
+                val presignedUrlResponse = presignedUrlResponseDeferred.await()
+                val requestBody = requestBodyDeferred.await()
+                aconAppRemoteDataSource.uploadFile(
+                    presignedUrlResponse.presignedUrl,
+                    requestBody
+                )
+
+                return@withContext presignedUrlResponse.fileUrl
+            }
+        }
+    }
+
+    private suspend fun Uri.getPresignedUrlResponse(imageType: ImageType): PresignedUrlResponse {
+        val fileName = DocumentFile.fromSingleUri(context, this)?.name
+            ?: error("Failed to read file name: $this")
+
+        return aconAppRemoteDataSource.getPresignedUrl(
+            GetPresignedUrlRequest(
                 imageType = imageType,
                 fileName = fileName
-            ))
+            )
+        )
+    }
 
-            val uriMimeType = context.contentResolver.getType(contentUri)
-            val finalMimeType = if (availableImageMimeTypes.contains(uriMimeType)) uriMimeType!! else "image/jpeg"
+    private fun Uri.getRequestBody(): RequestBody {
+        val uriMimeType = context.contentResolver.getType(this)
+        val finalMimeType = if (availableImageMimeTypes.contains(uriMimeType)) uriMimeType!! else "image/jpeg"
 
-            val inputStream = context.contentResolver.openInputStream(contentUri)
-            val requestBody = inputStream?.use { input ->
-                input.readBytes().toRequestBody(finalMimeType.toMediaTypeOrNull())
-            } ?: error("Failed to read image content: $url")
-
-            aconAppRemoteDataSource.uploadFile(presignedUrlResponse.presignedUrl, requestBody)
-
-            return@runCatchingWith presignedUrlResponse.fileUrl
-        }
+        val inputStream = context.contentResolver.openInputStream(this)
+        return inputStream?.use { input ->
+            input.readBytes().toRequestBody(finalMimeType.toMediaTypeOrNull())
+        } ?: error("Failed to read image content: $this")
     }
 }
